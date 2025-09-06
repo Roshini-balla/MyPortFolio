9c779d08d32bf959ac12f9e23f4209a8b7355551
Roshini-balla <roshinimighty98@gmail.com>
2025-09-06 15:56:21 +0530

    Update angular.json and other project changes

--- src/app/component/skills/skills.ts
+++ src/app/component/skills/skills.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
+  standalone: true, // <-- this line is important
+  imports: [CommonModule], // <-- add this line
   templateUrl: './skills.html',
   styleUrls: ['./skills.css']
 })
-export class SkillsComponent {}
+export class SkillsComponent {
+  skills = [
+    { icon: '🌐', name: 'HTML' },
+    { icon: '🎨', name: 'CSS' },
+    { icon: '🅰️', name: 'Angular' },
+    { icon: '☕', name: 'Java' },
+    { icon: '🌱', name: 'Spring Boot' },
+    { icon: '🛢️', name: 'MySQL' },
+    { icon: '🧠', name: 'Problem Solving' },
+    { icon: '🗃️', name: 'Git & GitHub' }
+  ];
+}
